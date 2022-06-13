7ff216743f7ed00a9353b72c307eb11ca030f376
HarishKamaras <harishkamarasu@gmail.com>
2022-06-13 16:04:53 +0530

    AddressBookManagement

---
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,58 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println(" Welcome to the Address Book Program");
+        Scanner scan = new Scanner(System.in);
+        AddressBookService addressBookServiceBook = new AddressBookService();
+        String character;
+        boolean loop =true;
+
+        while (loop) {
+            System.out.println(" ################################### MENU ###############################################");
+            System.out.println(" 1.) Add contacts\n 2.) Display contacts\n 3.) Edit contacts\n 4.) Delete contact\n"
+                    + " 5.) Add address book\n 6.) View contacts by city or state\n"
+                    + " 7.) Count contacts by city or state\n 8.) Sort contacts by city/state\n 9.) Exit");
+            System.out.println(" ########################################################################################");
+            System.out.print("\n Please enter your choice: ");
+
+            character = scan.next();
+
+            switch (character) {
+                case "1":
+                    addressBookServiceBook.addContact();
+                    break;
+                case "2":
+                    addressBookServiceBook.displayContacts();
+                    break;
+                case "3":
+                    addressBookServiceBook.editContact();
+                    break;
+                case "4":
+                    addressBookServiceBook.deleteContact();
+                    break;
+                case "5":
+                    addressBookServiceBook.addAddressBook();
+                    break;
+                case "6":
+                    addressBookServiceBook.viewByCityOrState();
+                    break;
+                case "7":
+                    addressBookServiceBook.countByContacts();
+                    break;
+
+                case "8":
+                    addressBookServiceBook.sortByContacts();
+                    break;
+                case "9":
+                    System.out.println(" EXIT ");
+                    loop = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice, please enter the valid choice");
+            }
+        }
+    }
+}
