7d253f046ff5b297b2fe29b29fee12acf28d06e1
HarishKamaras <harishkamarasu@gmail.com>
2022-06-20 19:17:16 +0530

    AddressBookManagement

--- src/main/java/com/bridgelabz/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -14,7 +14,7 @@ public class AddressBookMain {
             System.out.println(" ################################### MENU ###############################################");
             System.out.println(" 1.) Add contacts\n 2.) Display contacts\n 3.) Edit contacts\n 4.) Delete contact\n"
                     + " 5.) Add address book\n 6.) View contacts by city or state\n"
-                    + " 7.) Count contacts by city or state\n 8.) Sort contacts by city/state\n 9.) Exit");
+                    + " 7.) Count contacts by city or state\n 8.) Sort contacts by city/state\n 9.) File\n 10.)Exit");
             System.out.println(" ########################################################################################");
             System.out.print("\n Please enter your choice: ");
 
@@ -47,6 +47,21 @@ public class AddressBookMain {
                     addressBookServiceBook.sortByContacts();
                     break;
                 case "9":
+                    FileHandler file = new FileHandler();
+
+                    System.out.print(" Please enter to perform read or write: ");
+                    String fileOption = scan.next();
+
+                    if (fileOption.equals("read"))
+                        file.readFromFile();
+                    else {
+                        System.out.print(" Please enter which address book to write: ");
+                        String adBookFile = scan.next();
+
+                        file.writeIntoFile(adBookFile, addressBookServiceBook);
+                    }
+                    break;
+                case "10":
                     System.out.println(" EXIT ");
                     loop = false;
                     break;
---
+++ src/main/java/com/bridgelabz/FileHandler.java
@@ -0,0 +1,40 @@
+package com.bridgelabz;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileHandler {
+    public void readFromFile() {
+        Path path = Paths.get("src/main/resources/AddressBook.txt");
+
+        try {
+            System.out.println(Files.readAllLines(path));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void writeIntoFile(String adBookName, AddressBookService adBookHandler) {
+
+        AddressBook adBook = adBookHandler.findAddressBook(adBookName);
+        Path path = Paths.get("src/main/resources/AddressBook.txt");
+
+        try {
+            Files.write(path, adBook.addressBook.toString().getBytes());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
+
